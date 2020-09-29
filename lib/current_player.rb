188870a932f9323bdcd1c6940ed76413423e62a7
Tom Shephard <tom.shephard@hotmail.co.uk>
2020-09-29 14:08:46 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,16 +2,3 @@ def turn_count (board)
     count = 0
     puts count
 end
-
-
-
-
-def turn_counts (board)
-    count = 0
-    board.each do | board_position |
-      if board_position = "X" || board_position = "O"
-            count += 1
-            puts count
-    end
-    end
-end
